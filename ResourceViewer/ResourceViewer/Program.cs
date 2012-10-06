e34fedb4682ed61cb91be772f85e10100d1165b9
RoyAwesome <madKylegar@gmail.com>
2012-10-06 01:43:08 -0700

    If ran with two parameters (pack file and output directory) just unpack the files

--- ResourceViewer/ResourceViewer/Program.cs
+++ ResourceViewer/ResourceViewer/Program.cs
@@ -11,8 +11,21 @@ namespace ResourceViewer
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(String[] args)
         {
+            if (args.Length != 0)
+            {
+                string f = args[0];
+                if(f == "-help")
+                {
+                    Console.WriteLine("ResourceViewer.exe [packfile] [output directory]");
+                    return;
+                }
+                PackFile p = new PackFile(f);
+                p.WritePackToDirectory(args[1]);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
